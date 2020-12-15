ebf693c3541314e3bb622aed477a045153f4a1cf
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-15 22:24:51 +0100

    Full-screen, no DevTools

--- src/background.js
+++ src/background.js
@@ -13,13 +13,16 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    
+    /*
     width: 800,
     height: 600,
+    */
+    fullscreen: true,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      devTools: false
     }
   })
 

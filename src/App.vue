96ce896fd23ebc98a33ddd60f814038dcb15fa03
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-16 21:44:21 +0100

    Black background + video + quote, No full-screen + devtools

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,24 @@
   <div id="app">
     <!-- Part 2 - video and quote -->
     <!-- video -->
-    <video 
-      width="50%" 
-      height="50%"
-      autoplay loop preload="auto" 
-      playsinline
-    >
+    <video width="50%" height="50%" autoplay loop preload="auto" playsinline >
       <source src="../public/yeezy-ad.mp4" type="video/mp4">
-      Your browser does not support the video tag. 
     </video>
     <!-- message -->
-    <div>
-      <figure class="quote">
-        <blockquote>
-          '' {{message}} ''
-        </blockquote>
-        <figcaption>
-          &mdash; Kanye West, <cite>Vogue</cite>  </figcaption>
-      </figure>
-    </div>
+    <figure class="quote">
+      <blockquote>
+        '' {{message}} ''
+      </blockquote>
+      <figcaption>
+        &mdash; Kanye West, <cite>Vogue</cite>
+      </figcaption>
+    </figure>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
-  },
   data: function() {
     return {
       message: "Yeezy 350 V2 coming soon, get ready"
@@ -39,22 +29,21 @@ export default {
 </script>
 
 <style>
-body {
-  background-color:black
-}
-.quote {
-  margin: 5%;
-  background: #eee;
-  padding: 1em;
-  border-radius: 1em;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color:black
+  }
+  .quote {
+    margin: 5%;
+    background: #eee;
+    padding: 1em;
+    border-radius: 1em;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>

ca0fc03d176a70f48ae04709e0a3653dfd3f5d11
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-16 21:16:33 +0100

    Video + Quote

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- Part 2 - video and quote -->
+    <!-- video -->
     <video 
-      width="480px" 
-      height="270px"
+      width="50%" 
+      height="50%"
       autoplay loop preload="auto" 
       playsinline
     >
       <source src="../public/yeezy-ad.mp4" type="video/mp4">
       Your browser does not support the video tag. 
     </video>
+    <!-- message -->
+    <div>
+      <figure class="quote">
+        <blockquote>
+          '' {{message}} ''
+        </blockquote>
+        <figcaption>
+          &mdash; Kanye West, <cite>Vogue</cite>  </figcaption>
+      </figure>
+    </div>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
+  },
+  data: function() {
+    return {
+      message: "Yeezy 350 V2 coming soon, get ready"
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color:black
+}
+.quote {
+  margin: 5%;
+  background: #eee;
+  padding: 1em;
+  border-radius: 1em;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
